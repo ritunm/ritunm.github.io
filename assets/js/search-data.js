afd25d9870b041321939b42efcbd81c10480b43e
ritunm <raselritun1@gmail.com>
2025-04-24 20:55:59 +0000

    Deploying to gh-pages from @ ritunm/ritunm.github.io@60da6433961e95408035c9c58cb6e242258e4680 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,12 +423,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-5",
+          section: "News",},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
@@ -438,11 +433,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
@@ -453,6 +443,16 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-comprehensive-rust-bn-translated",
+          title: 'Comprehensive Rust - bn translated',
+          description: "This is the Rust course used by the Android team at Google. I have translated part of it into Bengali.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/comprehensive_rust_bn/";
+            },},{id: "projects-comprehensive-rust-bn",
+          title: 'Comprehensive Rust-bn',
+          description: "Desktop application for Comprehensive Rust-bn built using electron js.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/comprehensive_rust_desktop/";
             },},{id: "projects-depth-resnet-polyp-image-classification-tool",
           title: 'Depth-ResNet – Polyp Image Classification Tool',
           description: "DepthResNet – Polyp Image Classification Tool using Depth-ResNet (Cross-Platform)",
